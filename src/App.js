6d31a71726a10d1196130c0519ac80dce7221cdf
Makhnev, Andrey <andrey.makhnev@aliexpress.ru>
2021-06-03 15:27:10 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { Input } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dessertsByName: Object.assign({}, ...props.rows.map((row) => ({[row.name]: row}))),
+    };
+    this.desserts = props.rows.map((row) => (row.name));
+    this.classes = makeStyles({
+      table: {
+        minWidth: 650,
+      },
+    });
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.state.dessertsByName[event.target.id].portion = event.target.value;
+    this.setState({dessertsByName: this.state.dessertsByName});
+  }
+
+  render() {
+    return (
+      <TableContainer component={Paper}>
+        <Table className={this.classes.table} size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Dessert (100g serving)</TableCell>
+              <TableCell align="right">Calories</TableCell>
+              <TableCell align="right">Fat&nbsp;(g)</TableCell>
+              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              <TableCell align="right">Portion(g)</TableCell>
+              <TableCell align="right">Calories</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.desserts.map((name) => (
+              <TableRow key={name}>
+                <TableCell component="th" scope="row">
+                  {name}
+                </TableCell>
+                <TableCell align="right">{this.state.dessertsByName[name].calories}</TableCell>
+                <TableCell align="right">{this.state.dessertsByName[name].fat}</TableCell>
+                <TableCell align="right">{this.state.dessertsByName[name].carbs}</TableCell>
+                <TableCell align="right">{this.state.dessertsByName[name].protein}</TableCell>
+                <TableCell align="right">
+                  <Input
+                    id={name}
+                    value={this.state.dessertsByName[name].portion}
+                    onChange={this.handleChange}
+                    />
+                </TableCell>
+                <TableCell align="right">{this.state.dessertsByName[name].portion/100 * this.state.dessertsByName[name].calories}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  }
 }
 
-export default App;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein, portion:100};
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App rows={rows} />
   </React.StrictMode>,
   document.getElementById('root')
 );
